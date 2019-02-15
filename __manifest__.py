a78daedc40222e21ec4b2cb14695f0cab33ea74f
alex17121992 <alex_jm_1992@hotmail.com>
2019-02-15 17:53:25 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Control de Gastos e Ingresos',
+    'summary':"""Modulo de Contabilidad""",
+    'description': 'Modulo para controlar gastos e ingresos',
+    'author': 'Alejandro Jimenez Molina',
+    'depends': ['base'],
+    'application': True,
+    'category': 'Uncategorized',
+    'installable': True,
+    'data': ['Views/interfazGastos.xml','Views/interfazIngresos.xml','Views/entradaMenu.xml','Views/interfazDepartamento.xml','Views/interfazEmpleado.xml','Views/interfazGrafico.xml',
+             'Views/report.xml']
+}
